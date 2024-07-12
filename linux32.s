4438779baaae674f037f93fc40bb45c344c5ce1c
MXDMY <1628109656@qq.com>
2024-07-12 22:21:23 +0800

    项目提交

---
+++ linux32.s
@@ -0,0 +1,7 @@
+.equ SYS_EXIT ,  1
+.equ SYS_READ , 3
+.equ SYS_WRITE , 4
+.equ SYS_OPEN , 5
+.equ SYS_BRK , 45
+
+.equ TRAN_CHAR , '0' - 0
---
+++ run.sh
@@ -0,0 +1,5 @@
+as -32 -g -o start.o start.s
+ld -m elf_i386 -o start start.o
+./start r.txt w.txt 
+echo -e "\n\nExit Status Code:$?"
+rm -f start.o
